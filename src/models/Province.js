ec65ffe4f60874db8f764f55629854d272146f51
Malindi Chathumini <malindichathumini24@gmail.com>
2026-04-27 23:35:56 +0530

    docs: add schema documentation to Province model

--- src/models/Province.js
+++ src/models/Province.js
@@ -1,3 +1,7 @@
+// Province Model - stores Sri Lanka administrative province data
+// Contains name and unique code for each province
+// Referenced by District and TukTuk models
+
 import mongoose from 'mongoose';
 
 const provinceSchema = new mongoose.Schema({
